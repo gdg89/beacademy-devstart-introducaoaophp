75f7cc6b4bfbabe83493e223539db218f83635a0
Giuliano Garcia <giuilianogarcia2826@gmail.com>
2022-06-04 12:18:28 -0300

    Exercicio modulo 01 PHP

--- exercicio-crudContatos/index.php
+++ exercicio-crudContatos/index.php
@@ -5,17 +5,25 @@ include 'telas/head.php';
 include 'telas/menu.php';
 include 'acoes.php';
 
-if ($url === '/'){//compara se o que o cliente accesou a gente tem mapeado na plataforma.
-   home();
-}elseif($url === '/login'){
-    login();
-}elseif($url === '/cadastro'){
-   cadastro();
-}elseif($url === '/listar'){
-   listar();
+match($url){
+   '/'=> home(),
+   '/login'=> login(),
+   '/cadastro' => cadastro(),
+   '/listar' => listar(),
+   default => erro404(),
+};
 
-}else{
-   erro404();
-}
+// if ($url === '/'){//compara se o que o cliente accesou a gente tem mapeado na plataforma.
+//    home();
+// }elseif($url === '/login'){
+//     login();
+// }elseif($url === '/cadastro'){
+//    cadastro();
+// }elseif($url === '/listar'){
+//    listar();
+
+// }else{
+//    erro404();
+// }
 
 include 'telas/footer.php';
