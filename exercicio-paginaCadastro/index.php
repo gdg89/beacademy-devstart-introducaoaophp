70e6550a375c239acd654e6203cbdf28944353e1
Giuliano Garcia <giuilianogarcia2826@gmail.com>
2022-06-04 06:09:32 -0300

    incluindo arquivos head e footer e aplicando boostrap

--- exercicio-paginaCadastro/index.php
+++ exercicio-paginaCadastro/index.php
@@ -1,7 +1,7 @@
 <?php
 
 $url = $_SERVER['REQUEST_URI'];//Pega url que usuario accesou
-
+include 'telas/head.php';
 include 'telas/menu.php';
 
 if ($url === '/'){//compara se o que o cliente accesou a gente tem mapeado na plataforma.
@@ -13,3 +13,5 @@ if ($url === '/'){//compara se o que o cliente accesou a gente tem mapeado na pl
 }else{
     include 'telas/404.php';
 }
+
+include 'telas/footer.php';
